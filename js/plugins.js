6107d1ee0d603edff4e83360352c57c68bc89d5d
Muhammed-Saleh <hamada500005@gmail.com>
2018-08-23 03:35:22 +0300

    test02

--- js/plugins.js
+++ js/plugins.js
@@ -6,7 +6,7 @@
 
 // Set the date we're counting down to
 
-var countDownDate = new Date("dec 23, 2022 14:00:00").getTime();
+var countDownDate = new Date("dec 30, 2018 14:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
